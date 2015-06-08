14e5417207a61c4ce09c1f50a405ea5011ad3b55
dobri <dmd2169@columbia.edu>
2015-06-08 16:59:04 -0700

    get api for demo

--- SmallBackend.rb
+++ SmallBackend.rb
@@ -1,13 +1,21 @@
 require "sinatra"
 require "json"
 
+
+content = [{author: "Pete Hunt", text: "Wowza"}, {author: "Jordan Walke", text: "This is *another* comment"}]
+
 get '/' do
   'Hello world!'
 end
 
 get '/jsonp' do
   callback = params['callback']
-  content = {"golem" => "borec"}
-  content_type 'application/javascript'
+  
   "#{callback}(#{content.to_json})"
 end
+
+before do
+    content_type 'application/javascript'
+end
+
+
